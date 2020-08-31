03192a1a02c08c3283320d85bde1f0ed7b7c7d7c
Survivor75 <ashutoshnarang80@gmail.com>
2020-09-01 00:26:35 +0530

    observer pattern

--- patterns/strategy.py
+++ patterns/strategy.py
@@ -1,10 +1,6 @@
 """
-The strategy pattern is a behavioral software design pattern that enables selecting an algorithm at runtime. Instead of implementing a single algorithm directly, code receives run-time instructions as to which in a family of
-algorithms to use. Strategy lets the algorithm vary independently from clients that use it.
-
-For instance, a class that performs validation on incoming data may use the strategy pattern to select a validation algorithm depending on the type of data, the source of the data, user choice, or other discriminating factors.
-These factors are not known until run-time and may require radically different validation to be performed. The validation algorithms (strategies), encapsulated separately from the validating object, may be used by other
-validating objects in different areas of the system (or even different systems) without code duplication.
+Define a family of algorithms, encapsulate each one, and make them interchangeable.
+Strategy lets the algorithm vary independently from clients that use it.
 """
 
 import abc
